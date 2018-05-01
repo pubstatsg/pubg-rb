1a7b697e440d878db4ddc85c2f97063153bccf75
Dan Vera <arubinofaux@gmail.com>
2018-05-01 09:55:08 -0400

    rework status, i think i like this form better

--- lib/pubg/status.rb
+++ lib/pubg/status.rb
@@ -2,12 +2,11 @@ module PUBG
 	class Status
 		require "pubg/status/attributes"
 
-		attr_reader :type, :id, :attributes
+		attr_reader :data, :attributes
 
 		def initialize(args)
-			@type = args["data"]["type"]
-			@id = args["data"]["id"]
-			@attributes = Attributes.new(args["data"]["attributes"])
+			@data = args["data"]
+			@attributes = Attributes.new(@data["attributes"])
 		end
 	end
 end
